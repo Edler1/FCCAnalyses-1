ddf1dc2f77d88a4fe789a5222200503e26af92d8
github-action-benchmark <github@users.noreply.github.com>
2022-06-09 18:00:14 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for c1cb05b6c97913a82d099abed019b5ebf64db004

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1654797561494,
+  "lastUpdate": 1654797614301,
   "repoUrl": "https://github.com/Edler1/FCCAnalyses-1",
   "entries": {
     "Benchmark": [
@@ -68,6 +68,42 @@ window.BENCHMARK_DATA = {
             "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
             "value": 5.225951444835957,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "eploerer@lxplus7100.cern.ch",
+            "name": "Eduardo Ploerer"
+          },
+          "committer": {
+            "email": "eploerer@lxplus7100.cern.ch",
+            "name": "Eduardo Ploerer"
+          },
+          "distinct": true,
+          "id": "c1cb05b6c97913a82d099abed019b5ebf64db004",
+          "message": "adding coffea example",
+          "timestamp": "2022-06-09T19:37:00+02:00",
+          "tree_id": "6b586eef45f86938273c7cecfc623da13fcb0b8b",
+          "url": "https://github.com/Edler1/FCCAnalyses-1/commit/c1cb05b6c97913a82d099abed019b5ebf64db004"
+        },
+        "date": 1654797611490,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 16.145994901657104,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 20.797151803970337,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
           }
         ]
       }
