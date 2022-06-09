9d66e285d2f585c12c86bdc685d22293efe82434
github-action-benchmark <github@users.noreply.github.com>
2022-06-09 17:59:21 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for c1cb05b6c97913a82d099abed019b5ebf64db004

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1654797560136,
+  "lastUpdate": 1654797561494,
   "repoUrl": "https://github.com/Edler1/FCCAnalyses-1",
   "entries": {
     "Benchmark": [
@@ -36,6 +36,38 @@ window.BENCHMARK_DATA = {
             "unit": "Seconds",
             "range": 10,
             "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "eploerer@lxplus7100.cern.ch",
+            "name": "Eduardo Ploerer"
+          },
+          "committer": {
+            "email": "eploerer@lxplus7100.cern.ch",
+            "name": "Eduardo Ploerer"
+          },
+          "distinct": true,
+          "id": "c1cb05b6c97913a82d099abed019b5ebf64db004",
+          "message": "adding coffea example",
+          "timestamp": "2022-06-09T19:37:00+02:00",
+          "tree_id": "6b586eef45f86938273c7cecfc623da13fcb0b8b",
+          "url": "https://github.com/Edler1/FCCAnalyses-1/commit/c1cb05b6c97913a82d099abed019b5ebf64db004"
+        },
+        "date": 1654797561079,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 7.575770958091268,
+            "unit": "Evt/s"
+          },
+          {
+            "name": "nightly | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
+            "value": 5.225951444835957,
+            "unit": "Evt/s"
           }
         ]
       }
