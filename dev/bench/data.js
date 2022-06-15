7f90b186647d281af24c60c7d33aa4697c052aec
github-action-benchmark <github@users.noreply.github.com>
2022-06-15 17:18:53 +0000

    add Benchmark (customSmallerIsBetter) benchmark result for fa09d00657a6a7353f20549dcb16914f21de626d

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1654797617070,
+  "lastUpdate": 1655313533636,
   "repoUrl": "https://github.com/Edler1/FCCAnalyses-1",
   "entries": {
     "Benchmark": [
@@ -136,6 +136,42 @@ window.BENCHMARK_DATA = {
             "name": "prod | Events processed per second: examples/FCCee/flavour/Bc2TauNu",
             "value": 4.808350727185116,
             "unit": "Evt/s"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "eploerer@lxplus711.cern.ch",
+            "name": "Eduardo Ploerer"
+          },
+          "committer": {
+            "email": "eploerer@lxplus711.cern.ch",
+            "name": "Eduardo Ploerer"
+          },
+          "distinct": true,
+          "id": "fa09d00657a6a7353f20549dcb16914f21de626d",
+          "message": "Alternate GM compatible w/ FCCAnalyses",
+          "timestamp": "2022-06-15T19:09:42+02:00",
+          "tree_id": "d29f05b7b7902da01ee1ed85228bf0fd24587ed3",
+          "url": "https://github.com/Edler1/FCCAnalyses-1/commit/fa09d00657a6a7353f20549dcb16914f21de626d"
+        },
+        "date": 1655313531742,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/higgs/mH-recoil/mumu",
+            "value": 13.63434362411499,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/higgs/mH-recoil/mumu"
+          },
+          {
+            "name": "prod | Time spent running the analysis: examples/FCCee/flavour/Bc2TauNu",
+            "value": 18.357850551605225,
+            "unit": "Seconds",
+            "range": 10,
+            "extra": "Analysis path: examples/FCCee/flavour/Bc2TauNu"
           }
         ]
       }
